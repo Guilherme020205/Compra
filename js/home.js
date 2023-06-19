28f97129f649ed86598b49e724bd3ce78f3e2e8d
Guilherme Cardozo Seemann <seemannguilherme@gmail.com>
2023-06-19 11:41:20 -0300

    Criando as Funções de login

---
+++ js/home.js
@@ -0,0 +1,19 @@
+
+//login
+function MostrarSenha() {
+    var senha = document.getElementById("senha");
+    if (senha.type == "password") {
+        senha.type = "text";
+    } else {
+        senha.type = "password";
+    }
+}
+const form = document.querySelector('form');
+
+form.onsubmit = (e) => {
+
+    location.href = "index2.html";
+
+    return false;
+};
+//login
