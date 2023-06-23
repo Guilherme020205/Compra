71673ec6fb3b056896337030ed9b82b2619fbed6
Guilherme Cardozo Seemann <seemannguilherme@gmail.com>
2023-06-23 11:33:39 -0300

    Começando o boxNota

---
+++ js/pagProdutos/boxNota.js
@@ -0,0 +1,8 @@
+const lanches = ["Coxinha", "Risoles de Carne", "Esfirra de Pizza", "Salsicha Empanada",
+                "Espetinho de Frango", "Pastel de Carne", "Pastel de Frango"]
+const valorLanches = [4.00, 3.00, 5.00, 2.50, 5.00, 5.50, 5.50]
+const bebidas = ["Coca-Cola 2L", "Coca-Cola 350ML", "Coca-Cola 250ML", "Pureza 350ML", "Pureza 200ML",
+                "Àgua sem gás 1,5L", "Àgua com gás 1,5L"]
+const valorBebidas = [12.00, 5.00, 2.50, 4.50, 2.00, 3.50, 3.50]
+const extra = ["Salada de Frutas", "Bolo de Pote", "Picolé de Fruta"]
+const valorExtra = [4.00, 7.00, 3.50]
